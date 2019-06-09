05a800f33b5a1663a9ce81c13f27229e007a8e5b
Nisa Pınar Rüzgar <41566855+NisaRuzgar@users.noreply.github.com>
2019-06-09 20:41:05 +0300

    Add files via upload

---
+++ CENG431_HW1/src/presentation/AppPresentation.java
@@ -0,0 +1,18 @@
+/*
+Nisa Pinar Ruzgar 
+Asli Kul
+Eda Azin */
+package presentation;
+
+import java.util.ArrayList;
+
+import dataAccess.DataAccess;
+import domain.*;
+
+public class AppPresentation  {
+
+	public static void main(String[] args) {
+		Menu menu=new Menu();	
+	
+}
+}
